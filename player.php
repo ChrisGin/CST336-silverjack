00e7cd0a9f535beffe3ebb76d3841f16c63b5e5f
Samuel Villavicencio <savillavicencio@csumb.edu>
2016-02-27 16:28:58 -0800

    Randomly generate player names.

--- player.php
+++ player.php
@@ -1,6 +1,35 @@
 <?php
+    $names=array(''=>0);
 
-    function createPlayer($name, $pic)
+    function getName()
+    {
+        global $names;
+
+        while(true)
+        {
+            $counter=0;
+            $random=rand(0, count($names));
+
+            foreach( $names as $key => $value )
+            {
+                if( $counter == $random )
+                {
+                    if( $value == 0 )
+                    {
+                        $value=1;
+                        return $key;
+                    }
+
+                    else
+                        break;
+                }
+
+                $counter++;
+            }
+        }
+    }
+
+    function createPlayer($name)
     {
         $player=array('name' => $name, 'hand' => array_fill(0, 52, 0), 'score' => 0, 'pic' => $pic);
         return $player;
