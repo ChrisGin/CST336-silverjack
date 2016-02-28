a4070927635eb8f23e0098a9fa1fa60252dfcf1b
Karina Pizano <kpizano@csumb.edu>
2016-02-28 06:37:18 +0000

    NOT FIXED

--- index.php
+++ index.php
@@ -1,8 +1,44 @@
 <?php
+
+    $names=array('Jacqui' => 0, 'Kenshi' => 0, 'LiuKang' => 0, 'Raiden' => 0, 'Scorpion' => 0, 'SubZero' => 0,
+                 'Johnny' => 0, 'Kitana' => 0, 'Mileena' => 0, 'Reptile' => 0, 'Sonya' => 0, 'Tanya' => 0);
+
+    function getName()
+    {
+        global $names;
+        while(true)
+        {
+            $counter=0;
+            $random=rand(0, count($names));
+
+            foreach( $names as $key => $value )
+            {
+                if( $counter == $random )
+                {
+                    if( $value == 0 )
+                    {
+                        $value=1;
+                        return $key;
+                    }
+
+                    else
+                        break;
+                }
+
+                $counter++;
+            }
+        }
+    }
+
+    function createPlayer($name)
+    {
+        $player=array('name' => $name, 'hand' => array_fill(0, 52, 0), 'score' => 0, 'pic' => 'img/' . $name . '.png');
+        return $player;
+    }
         
-    //getHand($p4,$p4Score);
+    getHand($p4,$p4Score);
     
-    //$weener = getWeener($p1,$p2,$p3,$p4);
+    $weener = getWeener($p1,$p2,$p3,$p4);
     
     
     function getHand(&$player){
@@ -82,14 +118,9 @@
             </h1>
             <br />
             <br />
+            <br />
             <div class = "boxed">
-               hand<br />
-                hand<br />
-                 hand<br />
-                  hand<br />
-               hand<br />
-                hand<br />
-                 hand<br />
+              
             </div>
               
             
